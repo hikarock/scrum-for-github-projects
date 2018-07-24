c93c41efa77d45d539adfd98d60c284846ffe6b4
hikarock <i@hika69.com>
2018-07-24 21:06:22 +0900

    v0.3

--- contentScript.js
+++ contentScript.js
@@ -1,31 +1,61 @@
-setInterval(function() {
-  updateTotalLabel();
-}, 2000);
+const totalClassName = 'total-label';
+const columnsSelector = '.project-column';
+const labelsSelector = '.labels button';
+const columnTitleSelector = '.js-project-column-name';
 
-function updateTotalLabel() {
-  const totalClassName = 'total-label';
-  const columnsSelector = '.project-column';
-  const labelsSelector = '.labels button';
-  const columnTitleSelector = '.js-project-column-name';
-
+function updateTotalLabels() {
   let columns = document.querySelectorAll(columnsSelector);
   columns.forEach(column => {
     let labels = column.querySelectorAll(labelsSelector);
-    let total = 0;
-    labels.forEach(label => {
-      let text = label.textContent.trim();
-      if (text.match(/^([1-9]\d*|0)(\.\d+)?$/)) {
-        let point = parseFloat(text);
-        total += point;
-      }
-    });
-    let columnTitle = column.querySelector(columnTitleSelector);
-    if (column.querySelector(`.${totalClassName}`)) {
-      column.querySelector(`.${totalClassName}`).remove();
-    }
-    let totalLabel = document.createElement('span');
-    totalLabel.textContent = total;
-    totalLabel.classList = totalClassName;
-    columnTitle.appendChild(totalLabel);
+    let totals = calcTotals(labels);
+    removeTotalLabels(column);
+    appendTotalLabels(column, totals);
   });
 }
+
+function calcTotals(labels) {
+  let totals = {};
+  labels.forEach(label => {
+    let text = label.textContent.trim();
+    if (text.match(/^([1-9]\d*|0)(\.\d+)?$/)) {
+      if (totals[''] === undefined) {
+        totals[''] = 0;
+      }
+      totals[''] += parseFloat(text);
+    } else if (text.match(/^.*::([1-9]\d*|0)(\.\d+)?$/)) {
+      let matches = text.match(/^(.*)::([1-9]\d*|0)(\.\d+)?$/);
+      let key = matches[1];
+      let point = matches[2];
+      if (totals[key] === undefined) {
+        totals[key] = 0;
+      }
+      totals[key] += parseFloat(point);
+    }
+  });
+  return totals;
+}
+
+function appendTotalLabels(column, totals) {
+  let columnTitle = column.querySelector(columnTitleSelector);
+  for (let key in totals) {
+    let totalLabel = document.createElement('span');
+    if (key === '') {
+      totalLabel.textContent = totals[key];
+    } else {
+      totalLabel.textContent = `${key}::${totals[key]}`;
+    }
+    totalLabel.classList = totalClassName;
+    columnTitle.appendChild(totalLabel);
+  }
+}
+
+function removeTotalLabels(column) {
+  let totalLabels = column.querySelectorAll(`.${totalClassName}`);
+  totalLabels.forEach(totalLabel => {
+    totalLabel.remove();
+  });
+}
+
+setInterval(function() {
+  updateTotalLabels();
+}, 2000);
