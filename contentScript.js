560a45446ec846e4021ee82c7f6d407803fcc8db
hikarock <i@hika69.com>
2018-07-24 19:28:05 +0900

    v0.2

--- contentScript.js
+++ contentScript.js
@@ -1,21 +1,31 @@
-window.onload = function() {
-  setTimeout(function() {
-    let columns = document.querySelectorAll('.project-column');
-    columns.forEach(column => {
-      let labels = column.querySelectorAll('.labels button');
-      let total = 0;
-      labels.forEach(label => {
-        let text = label.textContent.trim();
-        if (text.match(/^\d+$/)) {
-          let point = parseInt(text);
-          total += point;
-        }
-      });
-      let columnTitle = column.querySelector('.js-project-column-name');
-      let totalLabel = document.createElement('span');
-      totalLabel.textContent = total;
-      totalLabel.classList = 'total-label';
-      columnTitle.appendChild(totalLabel);
+setInterval(function() {
+  updateTotalLabel();
+}, 2000);
+
+function updateTotalLabel() {
+  const totalClassName = 'total-label';
+  const columnsSelector = '.project-column';
+  const labelsSelector = '.labels button';
+  const columnTitleSelector = '.js-project-column-name';
+
+  let columns = document.querySelectorAll(columnsSelector);
+  columns.forEach(column => {
+    let labels = column.querySelectorAll(labelsSelector);
+    let total = 0;
+    labels.forEach(label => {
+      let text = label.textContent.trim();
+      if (text.match(/^\d+$/)) {
+        let point = parseInt(text);
+        total += point;
+      }
     });
-  }, 2000);
-};
+    let columnTitle = column.querySelector(columnTitleSelector);
+    if (column.querySelector(`.${totalClassName}`)) {
+      column.querySelector(`.${totalClassName}`).remove();
+    }
+    let totalLabel = document.createElement('span');
+    totalLabel.textContent = total;
+    totalLabel.classList = totalClassName;
+    columnTitle.appendChild(totalLabel);
+  });
+}
